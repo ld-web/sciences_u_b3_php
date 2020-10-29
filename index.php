e1168ee9d819bc625918e46edcbbf5b3d988ccbb
Lucas Delobelle <lucas@ld-web.net>
2020-10-29 10:44:26 +0100

    Constantes, types de variables

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 echo "Hello World<br />"; // Instruction echo affiche quelque chose à l'écran
 $numero = 1;
 
+// Constante : valeur non modifiable
+const MA_CONSTANTE = 4;
+
 /*
 Utilisation de guillemets doubles = variables interprétées
 Guillemets simples : variables non interprétées, utilisation de l'opérateur de concaténation
@@ -9,4 +12,13 @@ Guillemets simples : variables non interprétées, utilisation de l'opérateur d
 echo "Numéro : $numero<br />";
 echo '$numero<br />';
 echo 'Numéro : ' . $numero . '<br />';
-?>
+echo "Valeur de ma constante : " . MA_CONSTANTE . "<br />";
+
+$float = 9.23;
+echo $float . "<br />";
+
+$bool = false;
+echo "Valeur de mon booléen : " . $bool;
+// var_dump permet d'afficher le contenu d'une variable à l'écran
+// Différent de la fonction "echo", elle est davantage utilisée pour du débuggage
+var_dump($bool);
