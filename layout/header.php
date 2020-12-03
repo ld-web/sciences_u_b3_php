5012078e549fd1c7ac4640dd1e061c906fd0e0e1
Lucas Delobelle <lucas@ld-web.net>
2020-12-03 16:20:36 +0100

    Added nav in header

--- layout/header.php
+++ layout/header.php
@@ -9,3 +9,26 @@
 </head>
 
 <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Mes produits Sciences-U</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="/">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/products.php">Produits</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/newsletter_register.php">Newsletter</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/contact.php">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
